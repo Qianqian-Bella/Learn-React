7f564631596612dd5dba0e5b4c79edb002fc5a1b
Qianqian-Bella <892279435@qq.com>
2021-05-31 23:23:22 +0800

    project one class component finished

--- prj1_github-finder/src/App.js
+++ prj1_github-finder/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false, // if data has been fetched
     alert: null,
   };
@@ -41,9 +42,18 @@ class App extends Component {
   getUser = async (username) => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/users/${username}&client_id=${process.env.React_REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.React_REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}?client_id=${process.env.React_REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.React_REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     this.setState({ user: res.data, loading: false });
+  };
+
+  // Get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.React_REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.React_REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false });
   };
 
   // Clear users from state
@@ -60,7 +70,7 @@ class App extends Component {
   };
 
   render() {
-    const { loading, users, user } = this.state;
+    const { loading, users, user, repos } = this.state;
     return (
       <Router>
         <div className='App'>
@@ -86,7 +96,13 @@ class App extends Component {
                 )}
               />
               <Route exact path='/about' component={About} />
-              <Route exact path='/user/:login' render={(props) => <User {...props} getUser={this.getUser} user={user} loading={loading} />} />
+              <Route
+                exact
+                path='/user/:login'
+                render={(props) => (
+                  <User {...props} getUser={this.getUser} getUserRepos={this.getUserRepos} user={user} repos={repos} loading={loading} />
+                )}
+              />
             </Switch>
           </div>
         </div>
